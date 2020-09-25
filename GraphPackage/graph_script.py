9b157dab9f9bfbeb63b9f04a53f0386d273fe25c
DaviMagalhaesPereira <44785621+DaviMagalhaesPereira@users.noreply.github.com>
2020-09-25 10:47:50 -0300

    Buscas terminadas
    Buscas em lagura e profundidade finalizadas.

--- GraphPackage/graph_script.py
+++ GraphPackage/graph_script.py
@@ -4,13 +4,24 @@ from collections import deque
 class Graph:
 
     def __init__(self):
+        # self.V = 0
         self.graph = defaultdict(list)
 
     def addEdge(self, u, v):
+        # if (u not in self.graph) and (v not in self.graph):
+        #     self.V += 2
+        #     print("dois")
+        # else:
+        #     if (u in self.graph) or (v in self.graph):
+        #         self.V += 1
         self.graph[u].append(v)
+        # print(self.graph[v])
+        self.graph[v] = []
+        # print(self.graph)
+        # print(self.V)      
 
     def BFS(self, s):
-        visited = False * (len(self.graph))
+        visited = [False] * (len(self.graph))
         queue = deque()
         queue.append(s)
         visited[s] = True
@@ -25,20 +36,21 @@ class Graph:
                     visited[i] = True
     
     def DFS(self, s):
-        visited = False * (len(self.graph))
+        # print(self.graph)
+        # print(len(self.graph))
+        visited = [False] * (len(self.graph))
+        # print(visited)
         stack = []
         stack.append(s)
-        print(type(visited))
-        visited[s] = True
 
         while stack:
             s = stack[-1]
             stack.pop()
 
-            if visited[s] == False:  #not visited[s] talvez funcione também
+            if not visited[s]:  #not visited[s] talvez funcione também
                 print(s, end = " ")
                 visited[s] = True
 
             for i in self.graph[s]:
-                if visited[i] == False:  #not visited[i] talvez funcione também
+                if not visited[i]:  #not visited[i] talvez funcione também
                     stack.append(i)
--- main_script.py
+++ main_script.py
@@ -7,5 +7,17 @@ g.addEdge(2, 1)
 g.addEdge(0, 3) 
 g.addEdge(1, 4)
   
-print("Following is Depth First Traversal")  
+# print("Following is Depth First Traversal")  
 g.DFS(0)
+
+# g = graph_script.Graph() 
+# g.addEdge(0, 1) 
+# g.addEdge(0, 2) 
+# g.addEdge(1, 2) 
+# g.addEdge(2, 0) 
+# g.addEdge(2, 3) 
+# g.addEdge(3, 3) 
+  
+# print ("Following is Breadth First Traversal"
+#                   " (starting from vertex 2)") 
+# g.BFS(2)
