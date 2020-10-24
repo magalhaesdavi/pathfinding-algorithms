653160694d379e8e0579de55c6325a872cf24f99
Wagno <wagnoleao@gmail.com>
2020-10-24 16:37:36 -0300

    map generator atualizado para coordenadas

--- trabalhoIA/utils.py
+++ trabalhoIA/utils.py
@@ -1,7 +1,12 @@
 import random
 import numpy as np
+import math
+from itertools import combinations
 
-def map_Generator(available_nodes, density=0.5, weights_range=(0, 100), cartesian=True):
+def calculate_dist(cords1, cords2):
+    return math.sqrt( (cords1[0] - cords2[0])**2 + (cords1[1] - cords2[1])**2 )
+
+def map_generator(available_nodes, density=0.5, weights_range=(0, 100), cartesian=True):
     """
     Gera uma lista de conexoes com pesos no intervalo 'weights_range'
     baseado numa lista de ids em 'available_nodes', a variavel 'density'
@@ -9,24 +14,30 @@ def map_Generator(available_nodes, density=0.5, weights_range=(0, 100), cartesia
     """
     map_data = set({})
 
-    for _ in range(int(len(available_nodes) * density)):
-        nodes = random.sample(available_nodes, 2)
+    rand_cords = list(np.random.randint(weights_range[0], weights_range[1]+1, (len(available_nodes), 2)))
+    random.shuffle(available_nodes)
+    map_cords = list(map(lambda node_id, cords: (node_id, cords), available_nodes, rand_cords))
 
-        if not cartesian:
-            rand_weight = random.randint(weights_range[0], weights_range[1])
-        else:
-            cords1 = tuple(np.random.randint(weights_range[0], weights_range[1]+1, 2))
-            cords2 = tuple(np.random.randint(weights_range[0], weights_range[1]+1, 2))
-            rand_weight = (cords1, cords2)
+
+    connections = list(combinations(map_cords, 2))
+    random.shuffle(connections)
+
+    size = int(len(connections) * density)
+    if size > len(connections):
+        size = len(connections)
+
+    for connection in connections[: size]:
         
-        print(rand_weight)
+        conn_dist = calculate_dist(connection[0][1], connection[1][1])
+        noise = random.random() + 1
+
         map_data.add(
             (
-                nodes[0],
-                nodes[1],
-                rand_weight
+                (connection[0][0], tuple(connection[0][1])),
+                (connection[1][0], tuple(connection[1][1])),
+                round(conn_dist * noise, 3)
             )
         )
 
-    print(f"Mapa gerado com {int(len(available_nodes) * density)} conexoes")
+    print(f"Mapa gerado com {len(map_data)} conexoes")
     return map_data
